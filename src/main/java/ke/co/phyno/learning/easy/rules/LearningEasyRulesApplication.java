a57f2dac1ef6c200116d3fec508647476a6ca12d
Ochieng Onyango <phelix.ochieng@equitybank.co.ke>
2024-01-24 10:30:58 +0300

    Init

---
+++ src/main/java/ke/co/phyno/learning/easy/rules/LearningEasyRulesApplication.java
@@ -0,0 +1,11 @@
+package ke.co.phyno.learning.easy.rules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningEasyRulesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LearningEasyRulesApplication.class, args);
+    }
+}
---
+++ src/test/java/ke/co/phyno/learning/easy/rules/LearningEasyRulesApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.co.phyno.learning.easy.rules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningEasyRulesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
